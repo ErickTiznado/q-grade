7d0a0be23afe5852c3532a288d373590f7bbfde0
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-02-02 12:43:36 -0600

    Mejoras en la interfaz de el chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
 import React, { useState } from "react";
 import Header from "./components/Layout/Header/Header";
 import Sidebar from "./components/Layout/Sidebar/Sidebar";
-import ChatBox from "./components/ChatBox/ChatBox";
+import ChatWelcome from "./components/Chat/ChatWelcome";
+import ChatInput from './components/Chat/ChatInput'
 import "./App.css";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const [message, setMessage] =  useState('');
+const [showSidebar, setShowSidebar] = useState(true);
 
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
-
-  // Datos de ejemplo para el historial de chats
-  const chats = [
-    { id: 1, name: "Chat 1" },
-    { id: 2, name: "Chat 2" },
-  ];
-
-  const handleSelectChat = (chatId) => {
-    console.log("Chat seleccionado:", chatId);
-  };
 
   return (
-    <div className="parent">
-      {/* Sidebar */}
-      <div className="div1">
-        <Sidebar chats={chats} onSelectChat={handleSelectChat} />
+      <div className="app">
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+      <main className="main">
+        <div className="chat">
+          <ChatWelcome/>
+        </div>
+        <ChatInput message={message} setMessage={setMessage}/>
+      </main>   
       </div>
 
-      {/* Header */}
-      <div className="div2">
-        <Header onOpenModal={handleOpenModal} />
-      </div>
 
-      {/* Main Content */}
-      <div className="div3">
-        <ChatBox />
-      </div>
-    </div>
   );
 }
 
---
+++ src/components/UI/Q.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Q({className}){
+return(
+<img src="/src/assets/q.svg" alt="Q-Grade" className={className} />
+);
+}
