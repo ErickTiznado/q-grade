0d8cb2147b3ceb130456654058f7c348cce430a9
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2025-02-12 20:08:22 -0600

    Delete src directory

--- src/components/UI/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Logo({className}){
-return(
-<img src="/src/assets/Q-Grade.svg" alt="Q-Grade" className={className} />
-);
-}
--- src/components/UI/Q.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Q({className}){
-return(
-<img src="/src/assets/q.svg" alt="Q-Grade" className={className} />
-);
-}
